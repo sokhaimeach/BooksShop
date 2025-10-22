a938cbd7d161ac9ebe2822e79153eb99e2cec020
Meach Sokhai <dshhshh750@gmail.com>
2025-10-22 16:10:53 +0700

    create books report

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './Dashboard/dashboard/dashboard';
+import { Books } from './Dashboard/books/books';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    {path: 'dashboard', component: Dashboard}
+    {path: 'dashboard', component: Dashboard,
+        children: [
+            {path: '', redirectTo: 'books', pathMatch: 'full'},
+            {path: 'books', component: Books}
+        ]
+    }
 ];
